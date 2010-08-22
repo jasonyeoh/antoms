fe5834c5aa1e5f57c6b5222bf6cc2a1dd5c6399d
yogendrarampuria@gmail.com <yogendrarampuria@gmail.com@d89b995b-fad6-9c83-8961-43c7507a0cf1>
2010-08-22 10:32:08 +0000

    removed id from unique constraint

--- branches/dev/antoms/src/main/java/com/ant/oms/entity/UIChoice.java
+++ branches/dev/antoms/src/main/java/com/ant/oms/entity/UIChoice.java
@@ -8,7 +8,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.UniqueConstraint;
 @Entity
-@Table(uniqueConstraints={@UniqueConstraint(columnNames={"id","choice_id","choice_key", "locale"})})
+@Table(uniqueConstraints={@UniqueConstraint(columnNames={"choice_id","choice_key", "locale"})})
 public class UIChoice implements Serializable, BaseEntity {
 
 	/**
